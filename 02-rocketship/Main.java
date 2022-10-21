7a2a82f63f81936b66614227fdee31b99e665346
INAMMullah <inammu@nycstdents.net>
2022-10-21 01:20:41 -0400

    rocketship

---
+++ 02-rocketship/Main.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Rocket here.
+ *
+ * @author (Inamm,)
+ * @version (9/23/22)
+ */
+class Main {
+  public static void main(String[] args) {
+    Top();
+    Block();
+    Us();
+    Block();
+    Top();
+  }
+  public static void Top() {
+    System.out.println("      /  \\     ");
+    System.out.println("     /    \\     ");
+    System.out.println("    /      \\     ");
+  }
+  public static void Block() {
+    System.out.println("   +--------+  ");  
+    System.out.println("   |        |  ");
+    System.out.println("   |        |  ");
+    System.out.println("   +--------+  "); 
+  }
+    public static void Us() {
+    System.out.println("   | United |  ");
+    System.out.println("   | States |  ");
+  }
+}
+  
